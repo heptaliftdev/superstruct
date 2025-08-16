bfb94d2be5aef01ce85e11aef2f26937f50e4853
Hai Thanh Nguyen <vn.haint@gmail.com>
2025-08-16 12:58:27 +0700

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
-import typescript from "@rollup/plugin-typescript";
-import { defineConfig } from "rollup";
+import typescript from '@rollup/plugin-typescript'
+import { defineConfig } from 'rollup'
 
 export default defineConfig({
-  input: "./src/index.ts",
+  input: './src/index.ts',
   plugins: [typescript()],
   output: [
     {
-      file: "./dist/index.esm.js",
-      format: "esm",
+      file: './dist/index.esm.js',
+      format: 'esm',
       sourcemap: true,
-      assetFileNames: "[name].esm.js",
+      assetFileNames: '[name].esm.js',
     },
     {
-      file: "./dist/index.js",
-      format: "umd",
-      name: "Autolang",
+      file: './dist/index.js',
+      format: 'umd',
+      name: 'hyperstruct',
       sourcemap: true,
     },
   ],
-});
+})
